b558e3fd87d7e21fa99b5154e8024b133ae967b5
Ella Soccoli <112343747+esoccoli@users.noreply.github.com>
2022-12-10 14:20:34 -0500

    refactored solution

--- 2022/Day1/Program.cs
+++ 2022/Day1/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] input = File.ReadAllLines("../../../input.txt");
+
+            Part1(input);
+        }
+
+        static void Part1(string[] input)
+        {
+            List<int> elves = new List<int>();
+            int calsCarrying = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                
+                if (input[i] != "")
+                {
+                    calsCarrying += int.Parse(input[i]);
+                }
+                else
+                {
+                    elves.Add(calsCarrying);
+                    calsCarrying = 0;
+                }
+            }
+
+            int indexElfMaxCals = 0;
+
+            for (int elf = 0; elf < elves.Count; elf++)
+            {
+                if (elves[elf] > elves[indexElfMaxCals])
+                {
+                    indexElfMaxCals = elf;
+                }
+            }
+
+            Console.WriteLine("Part 1: " + elves[indexElfMaxCals]);
         }
     }
 }
