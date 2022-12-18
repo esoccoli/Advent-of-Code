b9e6ca6afb3fb269d26b6bd5cf86a248ce31d032
Ella Soccoli <112343747+esoccoli@users.noreply.github.com>
2022-12-17 21:57:11 -0500

    Trying to rework the solution to make it actually readable, currently in progress

---
+++ 2022/Day7/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Day7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("../../../input.txt");
+
+
+        }
+
+        static void Part1(string[] input)
+        {
+            long sumDirSizes = 0;
+
+            Stack<Directory> tree = new Stack<Directory>();
+            
+            foreach (string line in input)
+            {
+                if (line.StartsWith("$"))
+                {
+                    // is a command
+                    if (line.Equals("$ ls"))
+                    {
+                        foreach (Directory dir in tree)
+                        {
+                            List<Item> children = dir.Children;
+
+                            for (int i = 0; i < children.Count; i++)
+                            {
+
+                            }
+                        }
+                    }
+                    else if (line.Equals("$ cd /"))
+                    {
+                        Directory rootDir = new Directory();
+                        tree.Push(rootDir);
+                    }
+                    else if (line.Equals("$ cd .."))
+                    {
+                        tree.Pop();
+                    }
+                }
+            }
+        }
+    }
+}
